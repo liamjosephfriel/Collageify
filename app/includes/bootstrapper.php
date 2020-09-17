c01dfff03d99cc8fcbcb1c2ff715193b989f25f1
Liam Joseph Friel <liamjfriel@gmail.com>
2020-09-18 00:48:52 +0100

    Add twig structure, composer bootstrap, basic html layout of pages

--- app/includes/bootstrapper.php
+++ app/includes/bootstrapper.php
@@ -4,11 +4,16 @@
  */
 
 // Only load .env on dev environment
-if (!$_ENV['PROD_ENV']) {
-    $env_loader = Dotenv\Dotenv::createImmutable(__DIR__ . '../../');
+if (!isset($_ENV['PROD_ENV']) || !$_ENV['PROD_ENV']) {
+    $env_loader = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
     $env_loader->load();
 }
 
 // Twig
-$loader = new Twig_Loader_Filesystem($_ENV['APP_PATH'] . '/templates');
+$loader = new Twig_Loader_Filesystem($_ENV['APP_PATH'] . '/templates/');
 $twig = new Twig_Environment($loader);
+
+// Twig functions
+$twig->addFunction(new Twig_SimpleFunction('get_env', function($env) {
+    return getenv($env);
+}));
