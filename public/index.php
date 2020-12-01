61c784ffb1dba2250ad232d8c361df54e24770fd
Liam Joseph Friel <liamjfriel@gmail.com>
2020-12-01 00:40:30 +0000

    Remove debug INI settings

--- public/index.php
+++ public/index.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../app/includes/bootstrapper.php';
 
