7a7c8a8381398a2c51a481a40ff0186de5c29d1b
Liam Joseph Friel <liamjfriel@gmail.com>
2023-02-28 01:43:21 +0000

    Remove saving ability for collages to comply with Spotify API.
    Add Spotify logo to Collage to comply with Spotify API.
    
    Add list of albums and links to albums to comply with Spotify API.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,6 @@
 $('document').ready(function () {
 	$(window).scrollTop(0);
-	html2canvas(document.querySelector('#albumGrid'), { 'proxy': 'lib/proxy.php', 'logging': true, onclone: function () { } }).then(function (canvas) {
-		document.querySelector('#albumGrid').innerHTML = "";
-		document.querySelector('#albumGrid').appendChild(canvas);
-
-		$('#downloadButton').click(function () {
-			var download_button = document.getElementById('downloadButton');
-			download_button.href = canvas.toDataURL('image/jpeg').replace('image/jpeg', 'image/octet-stream');
-			download_button.download = "collage_" + Math.floor(Date.now() / 1000) + ".png";
-		});
-	});
-
+	
 	$('.term-button').click(function () {
 		$('#jsTermValue').val($(this).data('term'));
 		$('.jsTermForm').submit();
